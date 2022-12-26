e9879febe28cf80419b7b8296926ad4fc7050276
georgrybski <rybskigeorg@gmail.com>
2022-12-26 15:44:35 -0500

    Fixed app routing

--- final_project/server.py
+++ final_project/server.py
@@ -1,4 +1,4 @@
-from machinetranslation import translator
+from machinetranslation.translator import english_to_french, french_to_english
 from flask import Flask, render_template, request
 import json
 
@@ -7,13 +7,13 @@ app = Flask("Web Translator")
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    translated_text = translator.translate_english_to_french(textToTranslate)
+    translated_text = english_to_french(textToTranslate)
     return translated_text
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    translated_text = translator.translate_french_to_english(textToTranslate)
+    translated_text = french_to_english(textToTranslate)
     return translated_text
 
 @app.route("/")
